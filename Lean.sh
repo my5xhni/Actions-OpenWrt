9881621d28ec831b0c08e0cf5499d88af555cbb8
my5xhni <aishengyun@hotmail.com>
2020-06-23 00:41:16 +0800

    Update Lean.sh

--- Lean.sh
+++ Lean.sh
@@ -11,9 +11,12 @@ sed -i 's/192.168.1.1/192.168.11.44/g' package/base-files/files/bin/config_gener
 #移除不用软件包
 # rm -rf k3screenctrl package/lean
 #添加额外软件包
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+#git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 #ADH,passwall,smartdns,openclash
+git clone https://github.com/xnxy2012/luci-app-passwall.git package/luci-app-passwall
 git clone https://github.com/kenzok8/small package/small
 #passwall依赖
+git clone https://github.com/vernesong/OpenClash.git package/luci-openclash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
