94e2462040f0d1ed022c7cc0c29c62308790b6bb
my5xhni <aishengyun@hotmail.com>
2020-11-26 17:13:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git koolproxyR https://github.com/jefferymvp/luci-app-koolproxyR.git;master' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
+sed -i '$a rc-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
