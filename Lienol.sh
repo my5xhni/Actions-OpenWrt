18a421361cd244b677f083de6f7bc656db046951
my5xhni <aishengyun@hotmail.com>
2020-11-26 17:14:55 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -11,11 +11,12 @@ sed -i 's/192.168.1.1/192.168.11.44/g' package/base-files/files/bin/config_gener
 #移除不用软件包
 # rm -rf k3screenctrl package/lean
 #添加额外软件包
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
-git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone -b master https://github.com/vernesong/OpenClash.git  package/luci-app-OpenClash
-git clone https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssrp+
+# git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+# git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# git clone -b master https://github.com/vernesong/OpenClash.git  package/luci-app-OpenClash
+# git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+# git clone https://github.com/fw876/helloworld.git package/luci-app-ssrp+
+
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
