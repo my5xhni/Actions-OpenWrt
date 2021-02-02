385293e5b30b96c5271047dd8b77126f573b8f1a
my5xhni <aishengyun@hotmail.com>
2021-02-02 15:59:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@ sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;master' 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
 sed -i '$a src-git Twinzo1 https://github.com/Twinzo1/AdGuardHome.git;master' feeds.conf.default
 #small passwall dependence
-#sed -i '$a src-git small https://github.com/kenzok8/small;master' feeds.conf.default
-sed -i '$a src-git openwrt-passwall https://github.com/xiaorouji/openwrt-passwall;main' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
