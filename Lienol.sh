ccaa6b404593671c560caae2d88fb06ba7a8eee1
my5xhni <aishengyun@hotmail.com>
2021-02-08 14:34:39 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -12,6 +12,7 @@ sed -i 's/192.168.1.1/192.168.11.44/g' package/base-files/files/bin/config_gener
 # rm -rf k3screenctrl package/lean
 #添加额外软件包
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/Lienol/openwrt-OpenAppFilter.git package/luci-app-OpenAppFilter
 # git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
 # git clone -b master https://github.com/vernesong/OpenClash.git  package/luci-app-OpenClash
 # git clone https://github.com/kenzok8/openwrt-packages package/kenzo
