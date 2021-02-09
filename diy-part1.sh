2ad02cfd3ac215a272a660fe47e905a73ef90000
my5xhni <aishengyun@hotmail.com>
2021-02-09 16:40:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 #sed -i '$a src-git koolproxyR https://github.com/jefferymvp/luci-app-koolproxyR.git;master' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
+#sed -i '$a src-git openclash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
 sed -i '$a src-git Twinzo1 https://github.com/Twinzo1/AdGuardHome.git;master' feeds.conf.default
 #small passwall dependence
