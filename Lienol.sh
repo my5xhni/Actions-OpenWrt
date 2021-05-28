e0671820b7d6c1d6dc5acf00a02949cdd7636f69
my5xhni <aishengyun@hotmail.com>
2021-05-28 17:36:53 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -11,13 +11,16 @@ sed -i 's/192.168.1.1/192.168.11.44/g' package/base-files/files/bin/config_gener
 #移除不用软件包
 # rm -rf k3screenctrl package/lean
 #添加额外软件包
-git clone -b main   https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone -b master https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+
+# git clone -b main   https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 # git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# git clone -b master https://github.com/kenzok8/small.git package/small
+git clone -b master https://github.com/kenzok8/small.git package/small
 git clone -b master https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
-git clone -b master https://github.com/Lienol/openwrt-OpenAppFilter.git package/luci-app-OpenAppFilter
-git clone -b master https://github.com/Twinzo1/AdGuardHome.git package/luci-app-adguardhome2
+
+# git clone -b master https://github.com/Lienol/openwrt-OpenAppFilter.git package/luci-app-OpenAppFilter
+# git clone -b master https://github.com/Twinzo1/AdGuardHome.git package/luci-app-adguardhome2
 
 # git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
 # git clone -b master https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
